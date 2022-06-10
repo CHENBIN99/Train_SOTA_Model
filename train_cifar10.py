089bb012efaf6fa807d8c2eed25b3bff56189a7d
CHENBIN <30487978+CHENBIN99@users.noreply.github.com>
2022-06-10 14:26:38 +0800

    add more model

--- train_cifar10.py
+++ train_cifar10.py
@@ -92,18 +92,44 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 
 # Model
 print('==> Building model..')
-if args.net == 'vit-b':
-    net = timm.create_model('vit_base_patch16_224', pretrained=True)
+if args.net == 'resnet18':
+    net = timm.create_model('resnet18', pretrained=True, num_classes=10)
+elif args.net == 'resnet34':
+    net = timm.create_model('resnet34', pretrained=True, num_classes=10)
+elif args.net == 'resnet50':
+    net = timm.create_model('resnet50', pretrained=True, num_classes=10)
+elif args.net == 'wrn50-2':
+    net = timm.create_model('wide_resnet50_2', pretrained=True, num_classes=10)
+elif args.net == 'wrn101-2':
+    net = timm.create_model('wide_resnet101_2', pretrained=True, num_classes=10)
+elif args.net == 'inc_v3':
+    net = timm.create_model('inception_v3', pretrained=True, num_classes=10)
+elif args.net == 'inc_v4':
+    net = timm.create_model('inception_v4', pretrained=True, num_classes=10)
+elif args.net == 'bit50-3':
+    net = timm.create_model('resnetv2_50x3_bitm', pretrained=True, num_classes=10)
+elif args.net == 'bit101-3':
+    net = timm.create_model('resnetv2_101x3_bitm', pretrained=True, num_classes=10)
+elif args.net == 'bit152-4':
+    net = timm.create_model('resnetv2_152x4_bitm', pretrained=True, num_classes=10)
+elif args.net == 'vit-b':
+    net = timm.create_model('vit_base_patch16_224', pretrained=True, num_classes=10)
 elif args.net == 'vit-s':
-    net = timm.create_model('vit_small_patch16_224', pretrained=True)
+    net = timm.create_model('vit_small_patch16_224', pretrained=True, num_classes=10)
 elif args.net == 'vit-t':
-    net = timm.create_model('vit_tiny_patch16_224', pretrained=True)
+    net = timm.create_model('vit_tiny_patch16_224', pretrained=True, num_classes=10)
 elif args.net == 'deit-b':
-    net = timm.create_model('deit_base_patch16_224', pretrained=True)
+    net = timm.create_model('deit_base_patch16_224', pretrained=True, num_classes=10)
 elif args.net == 'deit-s':
-    net = timm.create_model('deit_small_patch16_224', pretrained=True)
+    net = timm.create_model('deit_small_patch16_224', pretrained=True, num_classes=10)
 elif args.net == 'deit-t':
-    net = timm.create_model('deit_tiny_patch16_224', pretrained=True)
+    net = timm.create_model('deit_tiny_patch16_224', pretrained=True, num_classes=10)
+elif args.net == 'swin-b':
+    net = timm.create_model('swin_base_patch4_window7_224', pretrained=True, num_classes=10)
+elif args.net == 'swin-s':
+    net = timm.create_model('swin_small_patch4_window7_224', pretrained=True, num_classes=10)
+elif args.net == 'swin-t':
+    net = timm.create_model('swin_tiny_patch4_window7_224', pretrained=True, num_classes=10)
 else:
     raise 'no matched model'
 
